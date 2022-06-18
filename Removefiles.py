f15b4b11a317047bb969a7fe8f68cefc458924ed
Poorva870 <80689366+Poorva870@users.noreply.github.com>
2022-06-18 12:52:12 +0530

    Add files via upload

---
+++ Removefiles.py
@@ -0,0 +1,11 @@
+import time
+import  os 
+import shutil
+
+path=input('Remove the files older than 1 month:')
+print(path)
+
+time.ctime([secs])
+ctime=os.start(path).st_ctime
+return ctime
+print(ctime)
